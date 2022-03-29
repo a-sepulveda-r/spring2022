6afe5cc17145d30b3ae4ada6ba1c12eb1c681b36
ariel <ariel@DESKTOP-FICRM1U>
2022-03-29 19:29:20 -0300

    commit

---
+++ EjemploSpring2022/src/main/java/cl/icap/fullstackjava2022/modulo5/EjemploSpring2022Application.java
@@ -0,0 +1,13 @@
+package cl.icap.fullstackjava2022.modulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpring2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpring2022Application.class, args);
+	}
+
+}
---
+++ EjemploSpring2022/src/test/java/cl/icap/fullstackjava2022/modulo5/EjemploSpring2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.icap.fullstackjava2022.modulo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpring2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
